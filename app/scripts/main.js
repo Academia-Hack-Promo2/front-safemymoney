85bd59f06881428eea4b7f531d8907926f23bfba
marco <maldonmarco@gmail.com>
2015-05-14 21:55:12 -0430

    Listar Transaction

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+ function transactions() {
+        $.ajax({
+            type: 'get',
+            url: 'http://localhost:3000/transactions',
+
+            success: function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    $('#xxxxxxxx').prepend('<li>' + data[i].date + data[i].description + data[i].t_type + data[i].amount + '</li>');
+                }
+            },
+            error: function(data) {
+                console.log(data);
+            }
+
+        });
+ }();
